cc00b1fc1b2b3febd45ee8fbac16ad5b78fec3a0
mwojtczak001 <120602318+mwojtczak001@users.noreply.github.com>
2024-06-24 22:00:05 -0400

    Create benefits.js

---
+++ benefits.js
@@ -0,0 +1,20 @@
+const benefitsArr = [
+  {
+    benefitTitle: 'Seize Control',
+    benefitDescription: 'Bring accuracy and accountability into your <span class="span-block">business</span>',
+    benefitImg: 'images/control-image.webp'
+  },
+  {
+    benefitTitle: 'Increase Efficiency',
+    benefitDescription: 'Let customers place and manage orders online at <span class="span-block">any time.  All data is captured in our platform and</span> processed through automated workflows.',
+    benefitImg: 'images/efficiency-image.webp'
+  },
+  {
+    benefitTitle: 'Stimulate Growth',
+    benefitDescription: 'Get the insight you need to forecast future events <span class="span-block"and ensure profitability.</span>',
+    benefitImg: 'images/growth-image.webp'
+  }
+]
+
+
+export default benefitsArr
