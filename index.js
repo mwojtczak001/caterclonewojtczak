f292029ded2b29611c88e72683e41cdc919a368d
mwojtczak001 <120602318+mwojtczak001@users.noreply.github.com>
2024-06-25 18:03:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,49 +8,13 @@ const welcomeModule = document.querySelector('.welcome-module')
 const closeModuleButton = document.querySelector('.close-module')
 const currentYearEl = document.querySelector('.current-year')
 const caterLogo = document.querySelector('.cater-logo')
-
+const statusColorImage = document.querySelector('.status-color-image')
+const siteStatusDescription = document.querySelector('.site-status-description')
 const leftBenefitsContainer = document.querySelector('.left-benefits-container')
+
 let benefitsHtml = ''
 
-benefitsArr.forEach((benefit) => {
-  benefitsHtml += `
-    <div class="benefit-container">
-      <img class="benefit-icon" src="${benefit.benefitImg}">
-      <div>
-        <h2>${benefit.benefitTitle}</h2>
-        <h4>${benefit.benefitDescription}</h4>
-      </div>
-    </div>
-  `
-})
-
-bookADemoBtn.addEventListener('click', () => {
-  location.href = 'bookdemo.html'
-})
-
-caterLogo.addEventListener('click', () => {
-  location.href = 'index.html'
-})
-
-leftBenefitsContainer.innerHTML = benefitsHtml
-
-document.querySelectorAll('.header-menu-item').forEach((menu) => {
-  menu.addEventListener('mouseover', () => {
-    menu.classList.add('hover-effect')
-  })
-
-  menu.addEventListener('mouseout', () => {
-    menu.classList.remove('hover-effect')
-  })
-})
-
-const currentYear = new Date().getFullYear()
-
-magnifyingGlassEl.addEventListener('mouseover', () => {
-  searchBarEl.style.display = 'inline-block'
-})
-
-supportChat.addEventListener('click', () => alert('Chat support'))
+// welcome module and associated functions/listeners 
 
 setTimeout(displayWelcomeMessage, 3000)
 
@@ -64,4 +28,60 @@ function closeModule() {
 
 closeModuleButton.addEventListener('click', closeModule)
 
+// Conditional for site display in footer 
+
+let siteOperational = true;
+
+if (siteOperational) {
+  statusColorImage.style.background = 'green'
+  siteStatusDescription.textContent = 'All Systems Operational'
+} else {
+  statusColorImage.style.background = 'red'
+  siteStatusDescription.textContent = 'System is currently offline'
+}
+
+// Rendering benefits information from benefits array 
+
+benefitsArr.forEach((benefit) => {
+  benefitsHtml += `
+    <div class="benefit-container">
+      <img class="benefit-icon" src="${benefit.benefitImg}">
+      <div>
+        <h2>${benefit.benefitTitle}</h2>
+        <h4>${benefit.benefitDescription}</h4>
+      </div>
+    </div>
+  `
+})
+
+leftBenefitsContainer.innerHTML = benefitsHtml
+
+bookADemoBtn.addEventListener('click', () => {
+  location.href = 'bookdemo.html'
+})
+
+caterLogo.addEventListener('click', () => {
+  location.href = 'index.html'
+})
+
+document.querySelectorAll('.header-menu-item').forEach((menu) => {
+  menu.addEventListener('mouseover', () => {
+    menu.classList.add('hover-effect')
+  })
+
+  menu.addEventListener('mouseout', () => {
+    menu.classList.remove('hover-effect')
+  })
+})
+
+const currentYear = new Date().getFullYear()
+
 currentYearEl.textContent = currentYear
+
+magnifyingGlassEl.addEventListener('mouseover', () => {
+  searchBarEl.style.display = 'inline-block'
+})
+
+supportChat.addEventListener('click', () => alert('Chat support'))
+
+
